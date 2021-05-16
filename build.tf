d2136fdb54c864d810f0e57123d74bc170427b47
mihasik69 <Hht378h191god!>
2021-05-16 12:09:48 +0300

    add files

---
+++ build.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.40.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "build" {
+  ami           = "ami-08962a4068733a2b6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Role = "worker"
+    Name = "worker"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-08962a4068733a2b6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Role = "web"
+    Name = "web"
+  }
+}
