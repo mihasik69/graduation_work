1360c530e2191af578d308d5344f730d97ccf6ce
mihasik69 <Hht378h191god!>
2021-05-16 12:58:33 +0300

    version 1.6

--- build.tf
+++ build.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "build" {
+resource "aws_instance" "worker" {
   ami           = "ami-08962a4068733a2b6"
   instance_type = "t2.micro"
 
