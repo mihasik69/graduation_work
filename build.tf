6d1feadcf9ad5dcd3ec97e21a1e19bcec34713d5
mihasik69 <Hht378h191god!>
2021-05-19 13:01:22 +0300

    version 1.16

--- build.tf
+++ build.tf
@@ -30,9 +30,6 @@ resource "aws_instance" "web" {
     Name = "web"
   }
 }
-resource "time_sleep" "wait_30_seconds" {
-  create_duration = "30s"
-}
 
 output "instance_web" {
   value = aws_instance.web.public_ip
@@ -42,19 +39,3 @@ output "instance_worker" {
   value = aws_instance.worker.public_ip
 }
 
-resource "null_resource" "ansible_hosts_provisioner" {
-  depends_on = [time_sleep.wait_30_seconds]
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash" ,"-c"]
-    command = <<EOT
-      export terraform_worcer_public_ip=$(terraform output instance_worker);
-      echo $terraform_staging_public_ip;
-      export terraform_web_public_ip=$(terraform output instance_web);
-      echo $terraform_production_public_ip;
-      sed -i -e "s/instance_worker/terraform_worker_public_ip/g" ./inventory/hosts;
-      sed -i -e "s/instance_web/terraform_web_public_ip/g" ./inventory/hosts;
-      sed -i -e 's/"//g' ./inventory/hosts;
-      export ANSIBLE_HOST_KEY_CHECKING=False
-    EOT
-  }
-}
